53f1bfa1607116d9294ab5209c3baaaa2395a1e8
SophiaMFerreira <sophiaagnes1004@gmail.com>
2026-09-07 19:16:31 -0300

    style: objetos posicionados corretamente na tirlha de inovacao

--- src/config/itensRegional.ts
+++ src/config/itensRegional.ts
@@ -3,7 +3,7 @@ export const posicoesItensLegislacao = [
     id: 1,
     top: "18.5%",
     left: "25.2%",
-  }, 
+  },
   {
     id: 2,
     top: "38.2%",
@@ -215,18 +215,18 @@ export const posicoesItensPropriedadeIntelectual = [
 export const posicoesItensAmbientesInovacao = [
   {
     id: 1,
-    top: "25.5%",
-    left: "16%",
+    top: "73%",
+    left: "81%",
   },
   {
     id: 2,
-    top: "19%",
-    left: "31%",
+    top: "46.6%",
+    left: "89.5%",
   },
   {
     id: 3,
-    top: "38%",
-    left: "45%",
+    top: "27%",
+    left: "79%",
   },
   {
     id: 4,
@@ -235,71 +235,66 @@ export const posicoesItensAmbientesInovacao = [
   },
   {
     id: 5,
-    top: "27%",
-    left: "79%",
+    top: "38%",
+    left: "45%",
   },
   {
     id: 6,
-    top: "44%",
-    left: "89%",
+    top: "19%",
+    left: "31%",
   },
   {
     id: 7,
-    top: "65%",
-    left: "81%",
+    top: "25.5%",
+    left: "16%",
   },
   {
     id: 8,
-    top: "71%",
-    left: "55%",
+    top: "54.9%",
+    left: "12.2%",
   },
   {
     id: 9,
-    top: "83%",
-    left: "33%",
+    top: "82.8%",
+    left: "15.5%",
   },
   {
     id: 10,
-    top: "74%",
-    left: "15%",
+    top: "92%",
+    left: "34%",
   },
   {
     id: 11,
-    top: "49%",
-    left: "12%",
-  },
-  {
-    id: 12,
-    top: "27%",
-    left: "16%",
+    top: "79.9%",
+    left: "55%",
   },
 ]
 
 export const posicoesTematicas = [
   {
-        id: 1,
-        top: "43%",
-        left: "25%",
-    },
-    {
-        id: 2,
-        top: "44%",
-        left: "56%",
-    },
-    {
-        id: 3,
-        top: "76%",
-        left: "25%",
-    },
-    {
-        id: 4,
-        top: "77%",
-        left: "55%",
-    },
+    id: 1,
+    top: "43%",
+    left: "25%",
+  },
+  {
+    id: 2,
+    top: "44%",
+    left: "56%",
+  },
+  {
+    id: 3,
+    top: "76%",
+    left: "25%",
+  },
+  {
+    id: 4,
+    top: "77%",
+    left: "55%",
+  },
 ]
 
 export const posicaoTarefaFinal = {
-    id: 5,
-    top: "60.4%",
-    left: "40.25%",
+  id: 5,
+  top: "60.4%",
+  left: "40.25%",
 };
